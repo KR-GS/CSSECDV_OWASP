816851228070e38895bb106e429542ce46670766
ReythemGespenst <roncajumban@gmail.com>
2025-05-30 17:24:02 +0800

    Updated main.py
    Main.py has all components as classes

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from pytm.pytm import TM, Datastore, Dataflow, Boundary, Actor, Lambda, Data, Classification
+from pytm.pytm import TM, Server, Datastore, Dataflow, Boundary, Actor, Lambda, Data, Classification
 
 tm = TM("Student Voter System")
 tm.description = "Activity 1 CSSECDV Group 5 - Cajumban, Larraquel, Pe, Santos, Singson"
-tm.isOrdered = True
 
 election_administrator = Actor("Election Administrator")
+
 voter = Actor("Voter")
+
 client_web_browser = Actor("Client Web Browser")
+
+server = Server("Server")
+
+student_voter_information = Datastore("Student Voter Information")
+
+etherium = Server("Etherium")
