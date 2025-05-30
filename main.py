bfc1eb4a80fc1e4e7b32bd184adcc254e91daf6c
ReythemGespenst <roncajumban@gmail.com>
2025-05-30 16:59:15 +0800

    updated main.py
    Added Actor classes for election administrator, voter, and client web browser

--- main.py
+++ main.py
@@ -2,4 +2,8 @@ from pytm.pytm import TM, Datastore, Dataflow, Boundary, Actor, Lambda, Data, Cl
 
 tm = TM("Student Voter System")
 tm.description = "Activity 1 CSSECDV Group 5 - Cajumban, Larraquel, Pe, Santos, Singson"
-tm.isOrdered = true
+tm.isOrdered = True
+
+election_administrator = Actor("Election Administrator")
+voter = Actor("Voter")
+client_web_browser = Actor("Client Web Browser")
