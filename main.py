8516a480d7e0f6e0b6359e0247e7725d1d76c767
ReythemGespenst <roncajumban@gmail.com>
2025-05-30 16:56:19 +0800

    Update main.py
    threat model has been initialized

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 from pytm.pytm import TM, Datastore, Dataflow, Boundary, Actor, Lambda, Data, Classification
+
+tm = TM("Student Voter System")
+tm.description = "Activity 1 CSSECDV Group 5 - Cajumban, Larraquel, Pe, Santos, Singson"
+tm.isOrdered = true
